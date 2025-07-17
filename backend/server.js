29a44e02497599b3175950ec09838f94f6e6e600
AnkitJain049 <2004.ankitjain@gmail.com>
2025-07-17 12:00:45 +0530

    Some production changes

--- backend/server.js
+++ backend/server.js
@@ -47,6 +47,7 @@ app.use(session({
   }),
   cookie: {
     secure: process.env.NODE_ENV === 'production',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     maxAge: 24 * 60 * 60 * 1000 // 24 hours
   }
 }));
